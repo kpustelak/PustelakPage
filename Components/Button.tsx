83e367e8de023156ff4ec32eec0d1903dc401768
Kornel Pustelak <92749743+kpustelak@users.noreply.github.com>
2026-08-05 00:29:34 +0200

    wip v2

--- Components/Button.tsx
+++ Components/Button.tsx
@@ -1,9 +1,40 @@
-import {Children, JSX} from "react";
+const colorMap = {
+  "edgy-black": {
+    primary: "text-white bg-edgy-black",
+    secondary: "text-edgy-black bg-white border border-edgy-black",
+  },
+  "normal-green": {
+    primary: "text-white bg-normal-green",
+    secondary: "text-normal-green bg-white border border-normal-green",
+  },
+  "black-green": {
+    primary: "text-white bg-black-green",
+    secondary: "text-black-green bg-white border border-black-green",
+  },
+  "sahara-green": {
+    primary: "text-white bg-sahara-green",
+    secondary: "text-sahara-green bg-white border border-sahara-green",
+  },
+  "jungle-green": {
+    primary: "text-white bg-jungle-green",
+    secondary: "text-jungle-green bg-white border border-jungle-green",
+  },
+} as const;
 
-export default function Button({children} : {children : JSX.Element}) {
-    return(
-        <button className="text-white bg-edgy-black h-12 px-10 rounded-xl text-sm">
-            {children}
-        </button>
-    );
+type BaseColor = keyof typeof colorMap;
+type ButtonType = "primary" | "secondary";
+
+export default function Button({
+  children,
+  type = "primary",
+  baseColor = "edgy-black",
+}: {
+  children: React.ReactNode;
+  type?: ButtonType;
+  baseColor?: BaseColor;
+}) {
+  const colorStyles = colorMap[baseColor][type];
+  const styles = `${colorStyles} h-12 px-10 rounded-xl text-sm`;
+
+  return <button className={styles}>{children}</button>;
 }
